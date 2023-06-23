aff5b630d0bd8d3a9a74e6ed722651ade4cc153d
Mario S J <mario2013sj@gmail.com>
2023-06-23 16:42:06 -0400

    chore: add Proposal and ProposalField models

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Proposal, ProposalField
 
 # Register your models here.
+
+
+class ProposalFieldInline(admin.TabularInline):
+    model = ProposalField
+    extra = 0
+
+
+@admin.register(Proposal)
+class ProposalAdmin(admin.ModelAdmin):
+    inlines = [ProposalFieldInline]
--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
+
 # Create your models here.
+class Proposal(models.Model):
+    STATUS_CHOICES = (
+        ("PENDING", "Pending"),
+        ("APPROVED", "Approved"),
+        ("DENIED", "Denied"),
+    )
+
+    # Campos do modelo de proposta
+    name = models.CharField(max_length=100)
+    # Adicione outros campos conforme necessário
+    status = models.CharField(
+        max_length=10, choices=STATUS_CHOICES, default="PENDING"
+    )
+    # Adicione campos de relacionamento, se necessário
+
+    def __str__(self):
+        return self.name
+
+
+class ProposalField(models.Model):
+    proposal = models.ForeignKey(
+        Proposal, on_delete=models.CASCADE, related_name="fields"
+    )
+    field_name = models.CharField(max_length=100)
+    field_value = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.field_name
